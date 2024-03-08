9a5e9b4db64515958170980a723818f7903b8cce
Bruno Umbelino <bruno.umbelino0@gmail.com>
2024-03-08 14:24:57 -0300

    Incrementando RotateArr

--- EstruturaDeDados_Algoritimos/Array/Rotate/RotateArrayImmutable.cs
+++ EstruturaDeDados_Algoritimos/Array/Rotate/RotateArrayImmutable.cs
@@ -5,7 +5,7 @@
         public void Execute()
         {
             var arr = new int[] { 1, 2, 3, 4, 5, 6 };
-            var result = RotateArray(arr, "left");
+            var result = RotateArray(arr, "right", 3);
 
             Console.WriteLine("Array: ");
             foreach (int i in arr)
@@ -20,29 +20,37 @@
             }
         }
 
-        private IEnumerable<int> RotateArray(int[] nums, string direction)
+        private IEnumerable<int> RotateArray(int[] nums, string direction, int times)
         {
             if (direction.ToLower() == "right")
             {
-                int aux = nums[nums.Length - 1];
-                for (int i = nums.Length - 1; i >= 0; i--)
+                while (times > 0)
                 {
-                    if (i == 0)
-                        nums[i] = aux;
-                    else
-                        nums[i] = nums[i - 1];
+                    int lastValue = nums[nums.Length - 1];
+                    for (int i = nums.Length - 1; i >= 0; i--)
+                    {
+                        if (i == 0)
+                            nums[i] = lastValue;
+                        else
+                            nums[i] = nums[i - 1];
+                    }
+                    times--;
                 }
                 return nums;
             }
             else if (direction.ToLower() == "left")
             {
-                int aux = nums[0];
-                for (int i = 0; i < nums.Length; i++)
+                while (times > 0)
                 {
-                    if (i == nums.Length-1)
-                        nums[i] = aux;
-                    else
-                        nums[i] = nums[i + 1];
+                    int firstValue = nums[0];
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        if (i == nums.Length - 1)
+                            nums[i] = firstValue;
+                        else
+                            nums[i] = nums[i + 1];
+                    }
+                    times--;
                 }
                 return nums;
             }
@@ -50,4 +58,4 @@
                 throw new ArgumentException();
         }
     }
-} 
+}
